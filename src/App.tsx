d8a6a48d217d4daf29a06f8d1b8feab57843d326
LukeVidalis <loukaslks@gmail.com>
2026-06-20 07:54:53 +0000

    chore: initial Tauri 2 + React + Tailwind scaffold

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "system-ui" }}>
+      <h1>STS2 Sync</h1>
+      <p>Loading…</p>
+    </div>
+  );
+}
+
+export default App;
